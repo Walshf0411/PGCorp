b4b12b63dec77534edb065be6feaff1d6950a737
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2018-03-10 16:29:22 +0530

    Sab Hogaya hai bas update view karna hai

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
-
+from .models import Flat
 app_name = 'homepage'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^flats/$', views.FlatView.as_view(), name='flats'),
+    url(r'^flats/(?P<pk>[0-9]+)/$', views.DetailView.as_view(model=Flat), name='detail'),
+    url(r'^flats/post/$', views.FlatCreate.as_view(model=Flat), name='flat-create'),
+    
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Flat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>This is the landing page</h1>')
+
+
+class FlatView(generic.ListView):
+    model = 'Flat'
+    template_name = 'homepage/flatlist.html'
+    context_object_name = 'flats'
+
+    def get_queryset(self):
+        return Flat.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = 'Flat'
+    template_name = 'homepage/detail.html'
+    context_object_name = 'flat'
+
+
+class FlatCreate(CreateView):
+    model = 'Album'
+    fields = ['nearest_railway_station', 'number_of_members', 'preferred_guests', 'price',
+              'price_negotiable', 'description']
+
+
+class FlatUpdate(UpdateView):
+    model = 'Album'
+    fields = ['nearest_railway_station', 'number_of_members', 'preferred_guests', 'price',
+              'price_negotiable', 'description']
+
+
